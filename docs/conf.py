25509f17612914f1bebda25b59ca54ce940838c6
Eugene Kim <ab@astral.blue>
2024-08-20 15:19:40 -0700

    feat: auto-pickup docs version from module

--- docs/conf.py
+++ docs/conf.py
@@ -3,13 +3,20 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+import resizewin
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'resizewin'
 copyright = '2024, Eugene Kim'
 author = 'Eugene Kim'
-release = 'resizewin.__version__'
+release = resizewin.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
